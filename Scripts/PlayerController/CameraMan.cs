8d0cfc396d1b4451d2ceb0749fdcef94d9663da7
J Y Kim <96564682+yun83@users.noreply.github.com>
2022-03-02 15:08:02 +0900

    Delete Scripts directory

--- Scripts/PlayerController/CameraMan.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CameraMan : MonoBehaviour
-{
-    public Transform CameraMain;
-    public Transform Axis;
-}
